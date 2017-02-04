8fb5e3880c3a79d16aff26ff72d9d28660c7bd29
Buck Rivard <buckrivard@gmail.com>
2017-02-03 16:30:24 -0800

    mainctrl updated, div management improvements

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -1,11 +1,16 @@
 (function () {
 	function MainCtrl($scope, Room, Message) {
 		$scope.rooms = Room.all;
-
+		$scope.activeRoom = null;
 		//$scope.getByRoomId = Message.getByRoomId;
 
 		$scope.addRoom = function(newName) {
-			Room.addRoom(newName);
+			if (/\S/.test(roomName)) {
+				Room.addRoom(newName);
+			} else {
+				return window.alert("Use your words, please!");	
+				//window alert to be replaced by alternate notification method. Eventually.
+			}
 			$scope.roomName = null;
 		}
 		$scope.selectRoom = function(room) {
--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -4,11 +4,7 @@
 		var rooms = $firebaseArray(ref);
 
 		var addRoom = function(roomName) {
-			if (/\S/.test(roomName)) {
 				rooms.$add({room: roomName});
-			} else {
-				return window.alert("Use your words, please!");	
-			}
 		};
 
 		return {
