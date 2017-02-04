44709ad077186d10d9a5465afb1ca8ed6041a67c
Buck Rivard <buckrivard@gmail.com>
2017-02-03 17:21:26 -0800

    addRoom = activeRoom ?

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -7,6 +7,13 @@
 		$scope.addRoom = function(newName) {
 			Room.addRoom(newName);
 			$scope.roomName = null;
+			/*
+			if (!$scope.activeRoom) {
+				$scope.selectRoom($scope.rooms - 1);
+			}
+			// What would I pass in here if I wanted the new room to become the active room when there is no activeRoom? 
+			// I.e. on page load? As it is now it requires creating the room then clicking it
+			*/
 		}
 		$scope.selectRoom = function(room) {
 			$scope.activeRoom = room;
